97871fa26f1176e193d89e1f1424330799e1e096
Alx <pokrovskiyalex77@gmail.com>
2023-05-16 14:34:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 123456;
+        int miles = 20;
+        int bonus = price / miles;
+
+        System.out.println("Стоимость билета:" + price);
+        System.out.println("Бонусных миль:" + bonus);
+    }
+}
